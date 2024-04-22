4b0b94caec5d7ec4ea5a22c75c44f00e9c89549f
B-e-sa <joaospereira963@gmail.com>
2024-04-22 04:31:35 -0300

    feat: adiciona troca de sprites da entidade passaro

--- entities/bird.py
+++ entities/bird.py
@@ -1,19 +1,67 @@
-import pygame
+import time
 
 class Bird:
-    def __init__(self, screen, sprite, ground_height):
-        self.gravity_force, self.y = 10, 0
+    def __init__(self, screen, sprites, ground_height):
+        # informacoes da tela para desenho do passaro
         self.screen = screen
         self.screen_height = screen.get_height()
-        self.sprite = sprite
-        self.sprite_rect = sprite.get_rect()
+        self.screen_width = screen.get_width()
         self.ground_height = ground_height
+        
+        # gravidade sob o passaro
+        self.gravity_force = .5
+        
+        # posicao x e y do passaro
+        self.x = self.screen_width / 3
+        self.y = (self.screen_height - self.ground_height) / 2
+        
+        # paint de sprites e respectivas logicas
+        self.sprites = sprites
+        self.current_sprite_index = 0
+        self.current_sprite_rect = self.sprites[self.current_sprite_index].get_rect()
+        self.last_sprite_change_time = time.time()
+        self.sprite_change_delay = .15
+        
+        # status do passaro
+        self.alive = True
+        #self.desired_height = self.y
+        #self.flap_height = self.gravity_force + 5
 
     def draw(self):
-        self.screen.blit(self.sprite, self.sprite_rect)
+        self.screen.blit(
+            self.sprites[self.current_sprite_index], 
+            (self.x, self.y)
+        )
+        
+    def change_sprite(self):
+        if not self.alive: return
+        
+        current_time = time.time()
+        not_in_sprite_change_delay = current_time - self.last_sprite_change_time >= self.sprite_change_delay
+        if not_in_sprite_change_delay:
+            self.current_sprite_index = (self.current_sprite_index + 1) % len(self.sprites)
+            self.sprite = self.sprites[self.current_sprite_index]
+            self.last_sprite_change_time = current_time
+
+    def flap(self):
+    #    self.desired_height += self.flap_height
+        return 
+    
+    
+    #def update_height(self):
+    #    if self.desired_height < self.y:
+    #        self.y += self.flap_height / 3
 
     def apply_gravity(self):
-        # mover isso para alguma função de morte
-        if self.y + self.sprite_rect.height > self.screen_height - self.ground_height: return
+        if not self.alive or self.desired_height > self.y: return
+        
+        is_bird_in_death_condition = (
+            self.y + self.current_sprite_rect.height 
+            > self.screen_height - self.ground_height
+        )
+        
+        if is_bird_in_death_condition: 
+            self.alive = False
+            return
+        
         self.y += self.gravity_force
-        self.sprite_rect.y = self.y
--- main.py
+++ main.py
@@ -27,13 +27,19 @@ scaled_bg_image = pygame.transform.scale(bg_sprite, (HEIGHT, WIDTH))
 background = Background(screen, scaled_bg_image, ground_sprite)
 
 # construção do pássaro
-bird_sprite = pygame.image.load('./sprites/bird/bird-midflap.bmp')
+downflap_sprite = pygame.image.load('./sprites/bird/bird-downflap.bmp')
+midflap_sprite = pygame.image.load('./sprites/bird/bird-midflap.bmp')
+upflap_sprite = pygame.image.load('./sprites/bird/bird-upflap.bmp')
+bird_sprites = [downflap_sprite, midflap_sprite, upflap_sprite]
+
 ground_height = ground_sprite.get_rect().height
-bird = Bird(screen, bird_sprite, ground_height)
+bird = Bird(screen, bird_sprites, ground_height)
 
 running = True
+paused = False
+
 while running:
-    # METODOS DE SAÍDA
+    # metodos de saida
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -41,13 +47,21 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         running = False
-
-    # APLICANDO GRAVIDADE AO PÁSSARO        
-    bird.apply_gravity()
         
-    # SPRITES
-    background.draw()
-    bird.draw()
+    # controles
+    if keys[pygame.K_w]:
+        bird.flap()
+        
+    if keys[pygame.K_PAUSE]:
+        paused = not paused
+
+    # atualizacao de sprites
+    if not paused:
+        background.draw()   
+        bird.draw()
+        bird.change_sprite()  
+        bird.apply_gravity()
+        # bird.update_height()
     
     pygame.display.flip()
     clock.tick(60)
