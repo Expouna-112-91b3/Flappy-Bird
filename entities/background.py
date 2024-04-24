b0162c65998b234ee3c58a5a4afb708cfe357461
B-e-sa <joaospereira963@gmail.com>
2024-04-23 22:42:50 -0300

    feat: adiciona logica de movimentacao do chao

--- entities/background.py
+++ entities/background.py
@@ -7,20 +7,29 @@ class Background:
         self.screen_width = screen.get_width()
         
         self.bg_sprite = bg_sprite
-        self.ground_sprite = ground_sprite
         
+        self.ground_sprite = ground_sprite
         self.ground_rect = self.ground_sprite.get_rect()
         self.ground_width = self.ground_rect.width
         self.ground_x = self.screen_height - self.ground_rect.height
+        
+        self.ground_movement_x = 0
     
     def draw(self):
         self.screen.blit(self.bg_sprite, (0, -self.ground_x))
         self.screen.blit(self.bg_sprite, (self.screen_height, -self.ground_x))
         
-        loop_size = 0
+        # o sprite do chao reseta ao chegar na metade do outro
+        # chao conectado a ele, de forma que ele nao acabe
+        # repentinamente
+        if self.ground_movement_x <= -self.screen_width:
+            self.ground_movement_x = 0
+        
         # quantidade de grounds conectados eh igual a
         # tamanho da tela / 312 (largura do ground em pixels - a sobreposicao entre eles)
         # arredondado para cima
-        for _ in range(math.ceil(self.screen_width / 312)):
-            self.screen.blit(self.ground_sprite, (loop_size, self.ground_x))
+        loop_size = 0
+        for _ in range(math.ceil(self.screen_width / 312) * 2):
+            self.ground_movement_x -= .5
+            self.screen.blit(self.ground_sprite, (loop_size + self.ground_movement_x, self.ground_x))
             loop_size = loop_size + self.ground_width
