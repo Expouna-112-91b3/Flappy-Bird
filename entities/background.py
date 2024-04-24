e86e881a0173f533ede0802b3a9d34ae098695d1
B-e-sa <joaospereira963@gmail.com>
2024-04-23 21:06:51 -0300

    fix: conserta posicionamento do background conforme altura do solo

--- entities/background.py
+++ entities/background.py
@@ -18,13 +18,13 @@ class Background:
         self.ground_x = self.screen_height - self.ground_rect.height
     
     def draw(self):
-        self.screen.blit(self.bg_sprite, (0, 0))
-        self.screen.blit(self.bg_sprite, (self.screen_height, 0))
+        self.screen.blit(self.bg_sprite, (0, -self.ground_x))
+        self.screen.blit(self.bg_sprite, (self.screen_height, -self.ground_x))
         
         loop_size = 0
-        # A QUANTIDADE DE GROUNDS CONECTADOS =
-        # tamanho da tela / 312 (largura do ground em pixels - a sobreposição entre eles)
-        # (arredondado para cima)
+        # quantidade de grounds conectados eh igual a
+        # tamanho da tela / 312 (largura do ground em pixels - a sobreposicao entre eles)
+        # arredondado para cima
         for _ in range(math.ceil(self.screen_width / 312)):
             self.screen.blit(self.ground_sprite, (loop_size, self.ground_x))
             loop_size = loop_size + self.ground_width
