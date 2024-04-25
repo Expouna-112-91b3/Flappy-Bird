dc50a6645b431e5a53db13f59418a080f0024909
B-e-sa <joaospereira963@gmail.com>
2024-04-25 00:58:48 -0300

    fix: remove codigo duplicado

--- entities/background.py
+++ entities/background.py
@@ -19,15 +19,18 @@ class Background:
         self.ground_movement_x = 0
 
     def draw(self):
+        """TODO
+        adicionar dinamicidade ao pintar background
+        """
         self.screen.blit(self.bg_sprite, (0, -self.ground_x))
         self.screen.blit(self.bg_sprite, (self.screen_height, -self.ground_x))
 
         """ 
         o sprite do chao reseta ao chegar na metade do outro
-        chao conectado a ele, de forma que ele nao acabe
-        repentinamente
+        chao conectado a ele, de forma que os sprites nao
+        acabem repentinamente
         """
-        if self.ground_movement_x <= -self.screen_width:
+        if self.ground_movement_x <= -self.screen_width / 2:
             self.ground_movement_x = 0
 
         """
@@ -35,16 +38,10 @@ class Background:
         tela / 312 (largura do ground em pixels - a sobreposicao entre eles)
         arredondado para cima
         """
-        # o sprite do chao reseta ao chegar na metade do outro
-        # chao conectado a ele, de forma que ele nao acabe
-        # repentinamente
-        if self.ground_movement_x <= -self.screen_width:
-            self.ground_movement_x = 0
-
         loop_size = 0
-        for _ in range(math.ceil(self.screen_width / 312) * 2 * 2):
-            self.ground_movement_x -= .5
-            self.ground_movement_x -= .5
+        for _ in range(math.ceil(self.screen_width / 312) * 2):
+            self.ground_movement_x -= .15
+            self.ground_movement_x -= .15
             self.screen.blit(self.ground_sprite, (loop_size + self.ground_movement_x +
                              self.ground_movement_x, self.ground_x))
             loop_size = loop_size + self.ground_width
