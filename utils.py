038499b48baf89e6a7783f78c078c527713ae247
B-e-sa <joaospereira963@gmail.com>
2024-05-07 04:36:54 -0300

    refactor: faz com que os metodos da classe utils sejam estaticos

--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
 import pygame
 
-"""
-Classe de utilitarios do codigo
-"""
-
 
 class Utils:
     colors = {
@@ -13,13 +9,13 @@ class Utils:
         "GREEN": (0, 255, 0),
         "BLUE": (0, 0, 255)
     }
-    
-    font = None
-    
-    def init_font(self):
-        pygame.font.init()
-        self.font = pygame.font.SysFont('Arial', 30)
 
-    def draw_font(self, screen, text, color=(0, 0, 0), pos = (0,0)):
-        text_surface = self.font.render(str(text), False, color)
+    @staticmethod
+    def init_font():
+        pygame.font.init()
+
+    @staticmethod
+    def draw_font(screen, text, color=(0, 0, 0), pos=(0, 0)):
+        font = pygame.font.SysFont('Arial', 30)
+        text_surface = font.render(str(text), False, color)
         screen.blit(text_surface, pos)
