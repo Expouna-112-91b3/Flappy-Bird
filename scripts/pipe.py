1527921f2af0ad6930f0c2fa1355ce59b83cc286
b_e_sa <joaospereira963@gmail.com>
2024-05-31 02:35:49 -0300

    refactor: troca movimentacao de pipes por move inves de apenas blit

--- scripts/pipe.py
+++ scripts/pipe.py
@@ -23,13 +23,8 @@ class Pipe:
         self.__pipe = self.__config.get_pipe()
         self.__sprite = self.__pipe["sprite"]["default"]
         self.__rotated_sprite = self.__pipe["sprite"]["rotated"]
-        self.__sprite_rect = self.__sprite.get_rect()
 
         self.__space_between_pipes = self.__bird_height * -6
-
-        self.__x_pos = 0
-        self.__x = self.__sprite_rect.width
-        self.__is_in_screen_limit = False
 
         self.__offset = randint(-self.__screen_height +
                                 self.__pipe["height"] - self.__space_between_pipes, 0)
@@ -37,66 +32,50 @@ class Pipe:
         self.__bottom_pipe_y = self.__offset + \
             self.__pipe["height"] - self.__space_between_pipes
 
-        """
-        por algum motivo o -10 e +2 são necessarios 
-        para calcular a posição da ponta dos canos
-        """
+        self.__x_pos = self.__screen_width
+        self.__speed = -5
+
+        self.__top_pipe_rect = self.__rotated_sprite.get_rect(
+            topleft=(self.__x_pos, self.__top_pipe_y))
+        self.__bottom_pipe_rect = self.__sprite.get_rect(
+            topleft=(self.__x_pos, self.__bottom_pipe_y))
+
         self.__hitbox = (
-            self.__top_pipe_y - 10 + self.__pipe["height"],
-            self.__bottom_pipe_y + 2
+            self.__top_pipe_y + self.__pipe["height"],
+            self.__bottom_pipe_y - 10
         )
 
+        self.__is_offscreen = False
+
     def check_collision(self, bird):
-        pygame.draw.rect(
-            self.__screen,
-            (0, 255, 0),
-            pygame.Rect(
-                self.__x_pos,
-                self.__hitbox[0],
-                self.__pipe["width"],
-                10), 1)
+        # hitboxes
+        # pygame.draw.rect(self.__screen, (255, 0, 0), self.__top_pipe_rect, 1)
+        # pygame.draw.rect(self.__screen, (255, 0, 0), self.__bottom_pipe_rect, 1)
 
-        pygame.draw.rect(
-            self.__screen,
-            (255, 0, 0),
-            pygame.Rect(
-                self.__x_pos,
-                self.__hitbox[1],
-                self.__pipe["width"],
-                10), 1)
+        bird_y_pos, bird_x_pos = bird.get_position()
 
-        bird_y_pos = bird.get_position()[0]
-        bird_x_pos = bird.get_position()[1]
-       
         # se o passaro estiver na mesma linha vertical dos canos
-        bird_inline_with_pipes = bird_y_pos >= self.__x_pos - self.__bird_width
-
-        print(bird_inline_with_pipes)
+        pipe_start = self.__top_pipe_rect.left - self.__bird_width
+        pipe_end = self.__top_pipe_rect.right + 6
+        bird_inline_with_pipes = pipe_start <= bird_y_pos <= pipe_end
 
         if bird_inline_with_pipes:
-            """
-            e se ele estiver mais alto que a ponta do cano superior
-            ou mais baixo que a ponta do cano inferior ele morre 
-            """
+            # se ele estiver mais alto que a ponta do cano superior ou mais baixo que a ponta do cano inferior, ele morre
             if (bird_x_pos <= self.__hitbox[0] or bird_x_pos >= self.__hitbox[1]):
                 bird.die()
 
     def draw(self):
+        move_distance = self.__speed
+        self.__top_pipe_rect = self.__top_pipe_rect.move(move_distance, 0)
+        self.__bottom_pipe_rect = self.__bottom_pipe_rect.move(
+            move_distance, 0)
+
         # checa se o cano está fora da tela
-        if not self.__is_in_screen_limit:
-            if (self.__x - self.__sprite_rect.width * 2 > self.__screen_width):
-                self.__is_in_screen_limit = True
+        if self.__top_pipe_rect.right < 0:
+            self.__is_offscreen = True
 
-        # posicao inicial dos canos, um pouco antes do inicio da tela
-        self.__x_pos = self.__screen_width - self.__x + self.__sprite_rect.width
+        self.__screen.blit(self.__rotated_sprite, self.__top_pipe_rect)
+        self.__screen.blit(self.__sprite, self.__bottom_pipe_rect)
 
-        self.__x += 5
-
-        self.__screen.blit(
-            self.__rotated_sprite,
-            (self.__x_pos, self.__top_pipe_y),
-        )
-        self.__screen.blit(
-            self.__sprite,
-            (self.__x_pos, self.__bottom_pipe_y),
-        )
+    def get_is_offscreen(self):
+        return self.__is_offscreen
