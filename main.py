74a2686aee01ea3e938bfe7058601eb71bb8ea11
b_e_sa <joaospereira963@gmail.com>
2024-05-31 02:34:16 -0300

    feat: sistema de geracao e delecao de pipes

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ config = Config()
 
 pygame.init()
 config.start_screen()
+config.setup_images()
 SCREEN = config.get_screen()["surface"]
 BACKGROUND = Background()
 BIRD = Bird()
@@ -32,48 +33,58 @@ dentro do loop do jogo, um for itera esses pipes e desenha cada um
 last_generation_time = time()
 generation_delay = 1.5
 pipes = []
-
-PIPE = Pipe()
+off_screen_pipes = []
 
 DEBUGGER = Debugger(BIRD)
 Utils.init_font()
+
 running, paused = True, False
 while running:
+    for pipe_index in off_screen_pipes:
+        del pipes[pipe_index]
+    off_screen_pipes.clear()
+
+    KEYS = pygame.key.get_pressed()
+
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if KEYS[pygame.K_PAUSE]:
+                paused = not paused
+
+            if KEYS[pygame.K_p]:
+                config.toggle_debug()
+
+            if not paused:
+                if KEYS[pygame.K_w]:
+                    BIRD.flap()
+
         if event.type == pygame.QUIT:
             running = False
 
-    KEYS = pygame.key.get_pressed()
     if KEYS[pygame.K_ESCAPE]:
-        running = False
-
-    if KEYS[pygame.K_PAUSE]:
-        paused = not paused
-
-    if KEYS[pygame.K_p]:
-        config.toggle_debug()
+                running = False
 
     if not paused:
         if KEYS[pygame.K_w]:
             BIRD.flap()
 
         BACKGROUND.draw_wallpaper()
-        PIPE.draw()
-        PIPE.check_collision(BIRD)
+        current_time = time()
+        not_in_generation_delay = current_time - \
+            last_generation_time >= generation_delay
+
+        if not_in_generation_delay:
+           last_generation_time = current_time
+           PIPE = Pipe()
+           pipes.append(PIPE)
+
+        for i, pipe in enumerate(pipes):
+            pipe.draw()
+            pipe.check_collision(BIRD)
+            if pipe.get_is_offscreen():
+                off_screen_pipes.append(i)
+
         BACKGROUND.draw_ground()
-
-        #current_time = time()
-        #not_in_generation_delay = current_time - \
-        #    last_generation_time >= generation_delay
-
-        #if not_in_generation_delay:
-        #   last_generation_time = current_time
-        #   PIPE = Pipe()
-        #    pipes.append(PIPE)
-
-        #for pipe in pipes:
-        #    pipe.draw()
-        #    pipe.check_collision(BIRD)
 
         BIRD.draw()
         BIRD.apply_gravity()
@@ -82,9 +93,10 @@ while running:
             paused = True
 
         if config.get_is_debugging():
-            DEBUGGER.draw_debug()
+            DEBUGGER.draw_debug(pipes)
 
     pygame.display.flip()
     config.clock_tick(60)
+
 
 pygame.quit()
