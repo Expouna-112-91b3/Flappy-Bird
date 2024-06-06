26a347fd3c39d870f305d2386d2bc0160993b162
b_e_sa <joaospereira963@gmail.com>
2024-06-06 20:50:50 -0300

    feat: melhor posicionamento das scores

--- scenes/score_board.py
+++ scenes/score_board.py
@@ -1,9 +1,9 @@
-from scripts.background import Background
-
 import pygame
 
 from config import Config
+
 from scripts.score import Score
+from scripts.background import Background
 
 from scenes.scenes import Scenes
 
@@ -15,18 +15,18 @@ class ScoreBoard():
         self.__score = Score()
 
         self.__screen = self.__config.get_screen()
-        self.__scren_width = self.__screen["width"]
-        self.__screen_height = self.__screen["height"]
+        self.__scren_width = self.__screen["width"] / 2
+        self.__screen_height = self.__screen["height"] / 2
         self.__surface: pygame.Surface = self.__screen['surface']
         self.__screen_rect = self.__screen['rect']
 
-        self.__board = pygame.Surface((550, 650))
+        self.__board = pygame.Surface((550, 750))
         self.__board.set_alpha(190)
         self.__board.fill((0, 0, 0))
+        self.__board_pos = self.__board.get_rect(center=self.__screen_rect.center)
+        self.__board_pos = (self.__board_pos.x, self.__board_pos.y - 50)
 
-        self.__board_pos = self.__board.get_rect(
-            center=self.__screen_rect.center)
-        self.__font = pygame.font.Font("./fonts/default.ttf", 50)
+        self.__font = pygame.font.Font("./fonts/default.ttf", 40)
 
     def run(self):
         KEYS = pygame.key.get_pressed()
@@ -47,9 +47,12 @@ class ScoreBoard():
         self.__surface.blit(self.__board, self.__board_pos)
 
         score_distance = 0
-        for player, score in self.__score.get_scores():
+        for i, (player, score) in enumerate(self.__score.get_scores()):
             text_surface = self.__font.render(
-                f"{player} - {score}", False, (255, 255, 255))
-            self.__surface.blit(text_surface, (self.__scren_width / 2 - text_surface.get_width(
-            ) / 2, self.__screen_height / 2 - 300 + score_distance))
-            score_distance += 50
+                f"{i + 1}. {player} - {score}", False, (255, 255, 255))
+
+            posx = self.__scren_width - text_surface.get_width() / 2
+            posy = self.__screen_height - 390 + score_distance
+
+            self.__surface.blit(text_surface, (posx, posy))
+            score_distance += 70
