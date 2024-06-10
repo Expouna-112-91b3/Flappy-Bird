9aac792d44f36246f02b19050850db53f5c5675f
b_e_sa <joaospereira963@gmail.com>
2024-06-10 16:55:32 -0300

    reafactor: score com somente as melhores 8 pontuacoes

--- scripts/score.py
+++ scripts/score.py
@@ -1,7 +1,10 @@
 from pygame.font import Font
 from pygame.surface import Surface
 
+from tools.utils import Utils
+
 from config import Config
+
 
 class Score:
     _instance = None
@@ -20,14 +23,14 @@ class Score:
             self.__surface: Surface = self.__config.get_screen()["surface"]
             self.__pos = (self.__surface.get_width() / 2, 50)
             self.__score = 0
-            self.__font = Font("./fonts/default.ttf", 30)
+            self.__font = Font("./fonts/default.ttf", 55)
 
             self.__current_score = 0
             self.__scores = []
-        
 
     def draw(self):
-        text_surface = self.__font.render(str(self.__current_score), False, (0, 0, 0))
+        text_surface = self.__font.render(
+            str(self.__current_score), False, Utils.Colors.WHITE.value)
         self.__surface.blit(text_surface, self.__pos)
 
     def increase(self):
@@ -35,18 +38,18 @@ class Score:
 
     def get_scores(self):
         return self.__scores
-    
+
     def get_score(self):
         return self.__score
-    
+
     def sort_score(self):
         self.__scores.sort(key=lambda x: x[1], reverse=True)
-        if len(self.__scores) > 10:
+        if len(self.__scores) > 8:
             self.__scores.pop()
 
     def push_score(self, player_name):
         self.__scores.append([player_name, self.__current_score])
         self.sort_score()
-    
+
     def reset_score(self):
         self.__current_score = 0
